8198f1c2054a73bbd016be21349c7a339849f4c7
Aditya Kumar <adityakumar9939@gmail.com>
2024-02-01 00:18:53 +0530

    udating UI a/c to weather

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,17 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUi = (data) => {
-    // console.log(data);
+    console.log(data);
     // const getcityDetls = data.getCityDetails;
     // const getWeatherDetls = data.getWeatherDetails;
 
     // destructure properties
     const { getCityDetails, getWeatherDetails } = data;
-    
+
     // update the details
     details.innerHTML = `
     <h5 class="my-3">${getCityDetails.EnglishName}</h5>
@@ -18,7 +20,20 @@ const updateUi = (data) => {
         <span>${getWeatherDetails.Temperature.Metric.Value}</span>
         <span>&deg;C</span>
     </div>
-    `
+    `;
+
+    // update the night and day img / icons
+    const iconSrc = `./img/icons/${getWeatherDetails.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = getWeatherDetails.IsDayTime ? './img/day.svg' : './img/night.svg';
+    // if (getWeatherDetails.IsDayTime) {
+    //     timeSrc = ;
+    // } else {
+    //     timeSrc = './img/night.svg';
+    // }
+    time.setAttribute('src', timeSrc);
+
     if (card.classList.contains('d-none')) {
         card.classList.remove('d-none');
     }
@@ -27,7 +42,7 @@ const updateUi = (data) => {
 const updateCity = async (city) => {
     const getCityDetails = await getCity(city);
     const getWeatherDetails = await getWeather(getCityDetails.Key);
-    
+
     return {
         getCityDetails,
         getWeatherDetails
@@ -42,6 +57,6 @@ cityForm.addEventListener('submit', e => {
 
     // update the UI with new city
     updateCity(city)
-        .then( data => updateUi(data))
-        .catch( err => console.log(err));
+        .then(data => updateUi(data))
+        .catch(err => console.log(err));
 });
